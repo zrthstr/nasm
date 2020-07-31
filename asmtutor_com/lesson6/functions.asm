9ddb8e25311d6888b4aac7d0d097ef2d3395099d
zrthstr <zrth1k@gmail.com>
2020-08-01 01:26:04 +0200

    ..

---
+++ asmtutor_com/lesson6/functions.asm
@@ -0,0 +1,58 @@
+;--------------------------------
+; int slen(String message)
+; String len calculation function
+slen:
+        push    ebx
+        mov     ebx, eax
+
+nexchar:
+        cmp     byte [eax], 0
+        jz      finished
+        inc     eax
+        jmp     nexchar
+
+finished:
+        sub     eax, ebx
+        pop     ebx
+        ret
+
+
+;--------------------------------
+; void sprint(String message)
+; String printing function
+sprint:
+        push    edx
+        push    ecx
+        push    ebx
+        push    eax
+        call    slen
+
+        mov     edx, eax
+        pop     eax
+
+                                ;
+                                ; ssize_t write(int fd, const void *buf, size_t count)
+                                ;
+        mov     ecx, eax
+        mov     ebx, 1
+        mov     eax, 4
+        int     80h
+
+        pop     ebx
+        pop     ecx
+        pop     edx
+        ret
+
+
+;--------------------------------
+; void exit()
+; Exit programm and restore resources
+quit:
+
+                                ;
+                                ; void _exit(int status);
+                                ;
+        mov     ebx, 0
+        mov     eax, 1
+        int     80h
+        ret
