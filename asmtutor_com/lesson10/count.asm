9e52ce79603cff13cfe413e01fcdc81488528c79
zrthstr <zrth1k@gmail.com>
2020-08-01 23:38:00 +0200

    ..

---
+++ asmtutor_com/lesson10/count.asm
@@ -0,0 +1,24 @@
+; print numbers from 1 to 10
+
+%include 'functions.asm'
+
+SECTION text
+global _start
+
+_start:
+        mov     ecx, 0
+
+nextNumber:
+        inc     ecx
+        mov     eax, ecx
+
+	add	eax, 48
+        push    eax
+        mov     eax, esp
+        call    sprint_line
+        cmp     ecx, 10
+        pop     eax
+        jne     nextNumber
+
+        call quit
+
