e7a97393d41eb9357743ec9b0143b10e437aea3b
zrthstr <zrth1k@gmail.com>
2020-08-04 00:28:59 +0200

    ..

--- asmtutor_com/lesson11/ccount.c
+++ asmtutor_com/lesson11/ccount.c
@@ -1,30 +1,39 @@
 // convert int to string
-// max N char for now
+// max 2*10**98
 
 #include <stdio.h>
+#include <unistd.h>
 
 
 int main(){
 	int test = 123987;
+	char buff[100] = {0};
+	char lf = '\n';
+
 	printf("Converting int %d\n", test);
 
-	char buff[100] = {0};
 	int i = 0;
-
-	for (;test>0;i++)  {
-		char rest = test % 10;
+	for (; test>0; i++){
+		buff[i] = (test % 10) + '0';
 		test = test / 10;
-		buff[i] = rest + 48; // '0'
-		//buff[i] = rest ;
-
-		printf("%d  %d\n",test, rest);
-		}
-	printf("buff has len: %ld", i);
-
-	printf("\n");
-	for (int c = 0; i>0; i--){
-		printf(" %x %c ",buff[i], buff[i] );
 	}
+
+	///////////////
+	// method #1 //
+	///////////////
+
+	for (int c=i; c>=0; c--){
+		write(1, &buff[c], 1);
+	}
+	write(1, &lf, 1);
+
+
+	///////////////
+	// method #2 //
+	///////////////
+
+	// tbd: reverse buff
+	write(1, &buff, i);
 
 	return 0;
 }
